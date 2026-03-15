543555235e7e3c84cfee2ba81e7358867b24c6f2
Jan Musial <s28977@pjwstk.edu.pl>
2026-03-16 00:18:28 +0100

    Added PrintHello method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        PrintHello();
+    }
+
+    private static void PrintHello()
+    {
         Console.WriteLine("Hello, World!");
     }
 }
