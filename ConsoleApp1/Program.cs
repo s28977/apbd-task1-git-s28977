98fbf5de550c0d81ff1b8e4fd680e02935f5da9e
Jan Musial <s28977@pjwstk.edu.pl>
2026-03-16 00:21:39 +0100

    Added username input

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,9 @@ class Program
     static void Main(string[] args)
     {
         PrintHello();
+        Console.WriteLine("Enter username:");
+        string userName = Console.ReadLine();
+        Console.WriteLine("Username is: " + userName);
     }
 
     private static void PrintHello()
