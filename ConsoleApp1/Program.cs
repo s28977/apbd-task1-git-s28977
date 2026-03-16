7e39a5cbaa586e3f368d57d6ed52a3b50a09a350
Jan Musial <s28977@pjwstk.edu.pl>
2026-03-16 01:52:32 +0100

    Changed username to name in Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,14 +6,14 @@ class Program
     {
         PrintHello();
         Console.WriteLine("Enter username:");
-        string username = Console.ReadLine();
-        if (string.IsNullOrWhiteSpace(username))
+        string name = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(name))
         {
             Console.WriteLine("Username cannot be empty.");
         }
         else
         {
-            Console.WriteLine("Hello " + username);
+            Console.WriteLine("Hello " + name);
         }
         int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         Console.WriteLine(StatisticsHelper.Sum(array));
